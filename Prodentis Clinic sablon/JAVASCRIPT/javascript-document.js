f0408a9790f429f8e26cdb8bd602bcf963f73e96
Alin-Stefan2002 <126024163+Alin-Stefan2002@users.noreply.github.com>
2023-10-17 17:10:41 +0300

    Add files via upload

---
+++ Prodentis Clinic sablon/JAVASCRIPT/javascript-document.js
@@ -0,0 +1,8 @@
+let subMenu=document.getElementById("subMenu");
+function showMenu(){
+   subMenu.classList.toggle("open-menu");
+}
+
+function deleteMenu(){
+   subMenu.classList.toggle("close-menu");
+}
